5a208ff93e03ed22f69e2c8f543be55873ebd62b
Aaron Gilman <gilmanaa2@gmail.com>
2019-07-30 18:05:26 +0300

    login, logout, and register added

--- billboard/SocialBoard/views.py
+++ billboard/SocialBoard/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, reverse
 from .models import Post
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
+@login_required
 def index(request):
     posts = Post.objects.all()
     context = {"posts":posts}
     return render(request,'SocialBoard/index.html',context)
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            login(request, new_user)
+            return HttpResponseRedirect(reverse("socialboard:index"))
+    else:
+        form = UserCreationForm()
+    return render(request,'registration/register.html',{"form":form})
