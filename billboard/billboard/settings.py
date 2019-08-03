6834722f0e544e30b4531b5c06ee7e139b576dd2
Aaron Gilman <gilmanaa2@gmail.com>
2019-08-03 15:11:09 +0300

    remove db stuff from before

--- billboard/billboard/settings.py
+++ billboard/billboard/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 from . import secret
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -159,6 +158,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 import dj_database_url
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
